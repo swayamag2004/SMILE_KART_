b2e1d3cdd7a93c33c208352684788548d213ae1c
apnacollege-demo <b522061@iiit-bh.ac.in>
2025-03-19 15:14:21 +0530

    Implemented localization to smile cart

--- src/Components/Cart/PriceCard.jsx
+++ src/Components/Cart/PriceCard.jsx
@@ -1,10 +1,13 @@
 import classNames from "classnames";
 import { Typography } from "neetoui";
 import { gt, keys } from "ramda";
+import { useTranslation } from "react-i18next";
 import useCartItemsStore from "stores/useCartItemsStore";
 import { Button } from "neetoui";
+import { Trans } from "react-i18next";
 import route from "routes";
 const PriceCard = ({ totalMrp, totalOfferPrice }) => {
+    const { t }=useTranslation();
   const totalDiscounts = totalMrp - totalOfferPrice;
   const isDiscountPresent = gt(totalDiscounts, 0);
   const discountPercentage = ((totalDiscounts / totalMrp) * 100).toFixed(1);
@@ -18,18 +21,27 @@ const PriceCard = ({ totalMrp, totalOfferPrice }) => {
           "line-through": isDiscountPresent,
         })}
       >
-        Total MRP: <span>${totalMrp}</span>
+        <Trans
+  components={{ typography: <span /> }}
+  i18nKey="totalMrp"
+  values={{ mrp: totalMrp }}
+/>
       </Typography>
       {isDiscountPresent && (
         <>
           <Typography className="flex justify-between text-green-700">
-            Total discounts:{" "}
-            <span>
-              ${totalDiscounts} ({discountPercentage}%)
-            </span>
+          <Trans
+          components={{ span: <span /> }}
+          i18nKey="totalDiscounts"
+          values={{ discounts: totalDiscounts, discountPercentage }}
+        />
           </Typography>
           <Typography className="flex justify-between">
-            Total offer price: <span>${totalOfferPrice}</span>
+          <Trans
+          components={{ span: <span /> }}
+          i18nKey="offerPrice"
+          values={{ offerPrice: totalOfferPrice }}
+        />
           </Typography>
           <span className="neeto-ui-text-gray-500 text-sm">
             {itemsCount} item(s)
@@ -39,7 +51,7 @@ const PriceCard = ({ totalMrp, totalOfferPrice }) => {
        <div className="flex flex-col items-center pt-4">
         <Button
           className="bg-neutral-800"
-          label="Buy now"
+          label={t("buyNow")}
           to={route.checkout}
         />
       </div>
