eda2906414644f888b159d6ef3a9145ad38064eb
YourGitHubUsernam <anshujatni22@gmail.com>
2025-03-22 15:12:04 +0530

    Adding

--- src/CheckoutPage.jsx
+++
@@ -1,9 +0,0 @@
-import "./CheckOutpage.css";
-import CheckOutForm from "./CheckOutForm"
-import CheckOutOverview from "./CheckOutOverview"
-const CheckOutpage=({arr})=>{
-    return <div className="whole">
-        <CheckOutForm className="a"/>
-        <CheckOutOverview className="a" arr={arr}/>
-    </div>
-}
